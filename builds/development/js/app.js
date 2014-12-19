b618045deef0e1d9e92e1be8430eeb59938e2d62
Virtual <jeanine.mt@gmail.com>
2014-12-19 12:19:16 -0700

    ch2 complete

---
+++ builds/development/js/app.js
@@ -0,0 +1,19 @@
+var myApp = angular.module('myApp', ['ngRoute', 'appControllers']);
+
+var appControllers = angular.module('appControllers',[]);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+		when('/login', {
+			templateUrl: 'views/login.html'
+		}).
+		when('/register', {
+			templateUrl: 'views/register.html'
+		}).
+		when('/meetings', {
+			templateUrl: 'views/meetings.html'
+		}).
+		otherwise({
+			redirectTo: '/login'
+		});
+}]);
