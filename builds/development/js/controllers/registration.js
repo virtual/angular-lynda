8790ca7ca30d5a265e536873381467180f69d2bf
Virtual <jeanine.mt@gmail.com>
2014-12-19 13:52:29 -0700

    ch3 complete

---
+++ builds/development/js/controllers/registration.js
@@ -0,0 +1,14 @@
+myApp.controller('RegistrationController', 
+
+function($scope, $location) {
+	$scope.login = function() {
+		$location.path('/meetings');
+	} // login
+	
+	$scope.register = function() {
+		$location.path('/meetings');
+	} // login
+	
+	
+	
+}); //RegistrationController
